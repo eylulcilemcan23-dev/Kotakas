41265569aa0cead18ea6a4589409da2d05206172
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-08-29 19:35:53 +0300

    feat(csharp-ui): load marketplace management module

--- csharp/Kotakas.Web/wwwroot/js/app.js
+++ csharp/Kotakas.Web/wwwroot/js/app.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const scripts=['/js/chunks/core.js','/js/chunks/marketplace.js','/js/chunks/payments.js','/js/chunks/deals.js','/js/chunks/admin-init.js'];
+  const scripts=['/js/chunks/core.js','/js/chunks/marketplace.js','/js/chunks/payments.js','/js/chunks/management.js','/js/chunks/deals.js','/js/chunks/admin-init.js'];
   for(const src of scripts){
     await new Promise((resolve,reject)=>{
       const s=document.createElement('script');
