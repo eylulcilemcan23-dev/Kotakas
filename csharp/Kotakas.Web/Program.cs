793422b98ca7307b975e30df6c0251eecfd10418
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-08-29 15:46:23 +0300

    feat(csharp): add modular ASP.NET Core entrypoint

---
+++ csharp/Kotakas.Web/Program.cs
@@ -0,0 +1,64 @@
+using Kotakas.Web.Api;
+using Kotakas.Web.Data;
+using Kotakas.Web.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+Directory.CreateDirectory(Path.Combine(builder.Environment.ContentRootPath, "App_Data"));
+
+builder.Services.AddDbContext<AppDbContext>(o =>
+    o.UseSqlite(builder.Configuration.GetConnectionString("Default") ?? "Data Source=App_Data/kotakas.db"));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(o =>
+{
+    o.User.RequireUniqueEmail = true;
+    o.Password.RequiredLength = 8;
+    o.Password.RequireUppercase = false;
+    o.Password.RequireLowercase = false;
+    o.Password.RequireDigit = false;
+    o.Password.RequireNonAlphanumeric = false;
+    o.Lockout.MaxFailedAccessAttempts = 8;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(o =>
+{
+    o.Cookie.Name = "kotakas.sid";
+    o.Cookie.HttpOnly = true;
+    o.Cookie.SameSite = SameSiteMode.Lax;
+    o.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    o.LoginPath = "/login.html";
+    o.AccessDeniedPath = "/login.html";
+    o.SlidingExpiration = true;
+    o.ExpireTimeSpan = TimeSpan.FromDays(7);
+    o.Events.OnRedirectToLogin = ctx => RedirectApi(ctx, 401);
+    o.Events.OnRedirectToAccessDenied = ctx => RedirectApi(ctx, 403);
+});
+
+builder.Services.AddAuthorization();
+var app = builder.Build();
+await StartupSeeder.InitializeAsync(app);
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapHealthEndpoints();
+app.MapAuthEndpoints();
+app.MapMarketplaceEndpoints();
+app.MapDealEndpoints();
+app.MapSupportEndpoints();
+app.MapAdminEndpoints();
+app.MapFallbackToFile("index.html");
+app.Run();
+
+static Task RedirectApi(RedirectContext<CookieAuthenticationOptions> ctx, int code)
+{
+    if (ctx.Request.Path.StartsWithSegments("/api")) { ctx.Response.StatusCode = code; return Task.CompletedTask; }
+    ctx.Response.Redirect(ctx.RedirectUri); return Task.CompletedTask;
+}
