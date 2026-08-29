cd0a8cf21b057cc79e2dc8e67be5d8206ebee086
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-08-29 03:49:52 +0300

    Ihtilaf ve audit guvenlik flaglerini ekle

--- src/config.js
+++ src/config.js
@@ -25,6 +25,8 @@ export const config = {
   escrowApiEnabled: envFlag('ESCROW_API_ENABLED', false),
   directEscrowEnabled: envFlag('DIRECT_ESCROW_ENABLED', false),
   marketWritesEnabled: envFlag('MARKET_WRITES_ENABLED', false),
+  disputeWritesEnabled: envFlag('DISPUTE_WRITES_ENABLED', false),
+  auditLogEnabled: envFlag('AUDIT_LOG_ENABLED', false),
   commissionRate: Number(process.env.COMMISSION_RATE || 0),
   traderDebtLimitGb: Number(process.env.TRADER_DEBT_LIMIT_GB || 0),
   googleClientId: process.env.GOOGLE_CLIENT_ID || '',
