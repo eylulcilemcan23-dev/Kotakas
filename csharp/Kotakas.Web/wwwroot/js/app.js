d0a67e9a300e968f398f89708703d04552ab64c8
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-08-31 02:36:47 +0300

    fix(csharp): refresh unified page bundle version

--- csharp/Kotakas.Web/wwwroot/js/app.js
+++ csharp/Kotakas.Web/wwwroot/js/app.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const UI_VERSION='20260831-0325';
+  const UI_VERSION='20260831-0340';
   const theme=document.createElement('link');
   theme.rel='stylesheet';
   theme.id='kotakasRetailTheme';
