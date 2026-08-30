f0450f997e1088d92915c6f92c6830ca2be47203
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-08-31 02:29:49 +0300

    fix(csharp): refresh visual-reference ui bundle

--- csharp/Kotakas.Web/wwwroot/js/app.js
+++ csharp/Kotakas.Web/wwwroot/js/app.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const UI_VERSION='20260831-0205';
+  const UI_VERSION='20260831-0245';
   const theme=document.createElement('link');
   theme.rel='stylesheet';
   theme.id='kotakasRetailTheme';
