a57068f60d775631c3ebf5061747f7fe0b743268
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-08-31 14:41:49 +0300

    Bump UI version for live chat auth fix

--- csharp/Kotakas.Web/wwwroot/js/app.js
+++ csharp/Kotakas.Web/wwwroot/js/app.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const UI_VERSION='20260831-1520-representative-live-chat';
+  const UI_VERSION='20260831-1535-live-chat-auth-fix';
 
   // Mobil Chrome/Android tarayıcı üst çubuğunu KOTAKAS pembe rengine boya.
   let themeColor=document.querySelector('meta[name="theme-color"]');
