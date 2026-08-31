370f5b0bfbb35423d55661531c7efa71eb31d55b
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-08-31 06:48:23 +0300

    Bump slider cache version

--- csharp/Kotakas.Web/wwwroot/js/app.js
+++ csharp/Kotakas.Web/wwwroot/js/app.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const UI_VERSION='20260831-1945';
+  const UI_VERSION='20260831-2015';
   const theme=document.createElement('link');
   theme.rel='stylesheet';
   theme.id='kotakasRetailTheme';
