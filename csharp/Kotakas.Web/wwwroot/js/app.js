0f5a7f6262eff47e95387abf5cdaf966d4843190
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-08-31 14:09:35 +0300

    Refresh mobile login icon styles

--- csharp/Kotakas.Web/wwwroot/js/app.js
+++ csharp/Kotakas.Web/wwwroot/js/app.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const UI_VERSION='20260831-1335-cart-dark';
+  const UI_VERSION='20260831-1410-login-mobile';
 
   // Mobil Chrome/Android tarayıcı üst çubuğunu KOTAKAS pembe rengine boya.
   let themeColor=document.querySelector('meta[name="theme-color"]');
