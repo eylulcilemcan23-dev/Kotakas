7380b4c46fe3e52f9ffaf37d87373a3a896af22d
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-08-31 17:24:44 +0300

    Bust cache for mobile drawer fixes

--- csharp/Kotakas.Web/wwwroot/js/app.js
+++ csharp/Kotakas.Web/wwwroot/js/app.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const UI_VERSION='20260831-1905-exact-game-covers-v4';
+  const UI_VERSION='20260831-1935-mobile-drawer-fix';
 
   // Mobil Chrome/Android tarayıcı üst çubuğunu KOTAKAS pembe rengine boya.
   let themeColor=document.querySelector('meta[name="theme-color"]');
