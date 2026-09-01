a87c63a926aea92a0ec346482445ded06ba16656
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-09-01 12:19:29 +0300

    Bump UI version for independent PUBG cover

--- csharp/Kotakas.Web/wwwroot/js/app.js
+++ csharp/Kotakas.Web/wwwroot/js/app.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const UI_VERSION='20260901-0646-pubg-kotakas-webp';
+  const UI_VERSION='20260901-1219-pubg-independent';
 
   // Mobil Chrome/Android tarayıcı üst çubuğunu KOTAKAS pembe rengine boya.
   let themeColor=document.querySelector('meta[name="theme-color"]');
