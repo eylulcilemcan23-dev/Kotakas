29d423633d247f07be2649dca378cb7a99557335
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-09-03 16:15:20 +0300

    Refresh simplified homepage assets

--- csharp/Kotakas.Web/wwwroot/js/app.js
+++ csharp/Kotakas.Web/wwwroot/js/app.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const UI_VERSION='20260903-1645-hide-hamburger';
+  const UI_VERSION='20260903-1715-home-cleanup';
 
   // Mobil Chrome/Android tarayıcı üst çubuğunu KOTAKAS pembe rengine boya.
   let themeColor=document.querySelector('meta[name="theme-color"]');
