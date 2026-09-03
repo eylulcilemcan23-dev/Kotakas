64898de8970210cbc3abfbee282f69de07f2b6fb
eylulcilemcan23-dev <eylulcilemcan.23@gmail.com>
2026-09-03 16:27:39 +0300

    Bump UI cache for final footer cleanup

--- csharp/Kotakas.Web/wwwroot/js/app.js
+++ csharp/Kotakas.Web/wwwroot/js/app.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const UI_VERSION='20260903-1805-footer-clean-align';
+  const UI_VERSION='20260903-1835-footer-final';
 
   // Mobil Chrome/Android tarayıcı üst çubuğunu KOTAKAS pembe rengine boya.
   let themeColor=document.querySelector('meta[name="theme-color"]');
